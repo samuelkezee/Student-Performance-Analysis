3b41c3549034557be1bc1e43a45c1f526665e00a
samuelkezee <samuelsam2k27@gmail.com>
2025-12-03 19:58:41 +0530

    predict pipeline added

--- src/utils.py
+++ src/utils.py
@@ -33,7 +33,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
 
             #hyper parameter tuning 
             gs=GridSearchCV(model,para,cv=3)
-            
+
             gs.fit(X_train,y_train)
 
 
@@ -55,5 +55,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
         
     except  Exception as e:
         raise CustomException(e,sys)
-        
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
     
